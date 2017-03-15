0600abbe4c0a49cf294eec2060c20cb7e76ac3e5
Scott Goerzen <sgoerzen@uvic.ca>
2017-03-15 10:18:17 -0700

    Added seperate method to generate new tile for added AI features and capabilities. Also added remove for tiles after player has passed.

---
+++ Assets/Scripts/NewTile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewTile : MonoBehaviour {
+
+	public GameObject tilePrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+	public GameObject SpawnTile()
+	{
+		GameObject go;
+		go = Instantiate (tilePrefab) as GameObject;
+		return go;
+	}
+
+}
